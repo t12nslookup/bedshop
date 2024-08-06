42f243d79bf7bd5ba3e9dde2c278cef64cd29d0d
Jon Adams <t12nslookup@gmail.com>
2024-08-06 20:43:15 +0100

    rubocop -A

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,21 +12,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_07_152229) do
-
-  create_table "mattresses", force: :cascade do |t|
-    t.integer "shop_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["shop_id"], name: "index_mattresses_on_shop_id"
+ActiveRecord::Schema.define(version: 20_230_607_152_229) do
+  create_table 'mattresses', force: :cascade do |t|
+    t.integer 'shop_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['shop_id'], name: 'index_mattresses_on_shop_id'
   end
 
-  create_table "shops", primary_key: "shop_id", force: :cascade do |t|
-    t.text "shop", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["shop"], name: "index_shops_on_shop"
+  create_table 'shops', primary_key: 'shop_id', force: :cascade do |t|
+    t.text 'shop', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['shop'], name: 'index_shops_on_shop'
   end
 
-  add_foreign_key "mattresses", "shops", primary_key: "shop_id"
+  add_foreign_key 'mattresses', 'shops', primary_key: 'shop_id'
 end
